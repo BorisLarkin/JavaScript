d5b1a4376dd5967701dc666b7f673d68565e4708
ITManiac <borislarkin18@mail.ru>
2024-03-12 23:24:41 +0300

    L3 fixed containers

--- L3/components/product-card/index.js
+++ L3/components/product-card/index.js
@@ -2,23 +2,23 @@ export class ProductCardComponent{
     constructor(parent){
         this.parent = parent;
     }
-    getHTML() {
+    getHTML(data) {
         return (
             `
                 <div class="card" style="width: 300px;">
-                    <img class="card-img-top" src="https://i.pinimg.com/originals/c9/ea/65/c9ea654eb3a7398b1f702c758c1c4206.jpg" alt="картинка">
+                    <img class="card-img-top" src="${data.src}" alt="картинка">
                     <div class="card-body">
-                        <h5 class="card-title">Акция</h5>
-                        <p class="card-text">Вот тут информация об акции</p>
-                        <button class="btn btn-primary"">Нажми на меня</button>
+                        <h5 class="card-title">${data.title}</h5>
+                        <p class="card-text">${data.text}</p>
+                        <button class="btn btn-primary">Нажми на меня</button>
                     </div>
                 </div>
             `
         )
     }
     
-    render() {
-        const html = this.getHTML()
+    render(data) {
+        const html = this.getHTML(data)
         this.parent.insertAdjacentHTML('beforeend', html)
     }
 }
