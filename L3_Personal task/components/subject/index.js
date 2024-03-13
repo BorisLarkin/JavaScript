3add5915faaff86506199e4d9062777417a3581d
ITManiac <borislarkin18@mail.ru>
2024-03-13 23:18:21 +0300

    Update index.js

--- L3_Personal task/components/subject/index.js
+++ L3_Personal task/components/subject/index.js
@@ -6,38 +6,39 @@ export class SubjectComponent {
         /*
         const result1 = 
         `
-            <div class="card mb-3" style="width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-                            <div class="carousel-inner">
+                <div class="card mb-3" style="width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                                <div class="carousel-inner">
         `
         const result2 = 
         `
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade"  data-bs-slide="prev">
+                                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                  <span class="visually-hidden">Предыдущий</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade"  data-bs-slide="next">
+                                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                  <span class="visually-hidden">Следующий</span>
+                                </button>
                             </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade"  data-bs-slide="prev">
-                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                              <span class="visually-hidden">Предыдущий</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade"  data-bs-slide="next">
-                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                              <span class="visually-hidden">Следующий</span>
-                            </button>
                         </div>
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">${data.title}</h5>
-                            <p class="card-text">${data.text}</p>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">${data.title}</h5>
+                                <p class="card-text">${data.text}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
         `
-        content = 
+        const content = 
         `
         <div class="carousel-item active">
         `
+        
         data.src.forEach((item) => {
             content +=
             `
