582994132ffac8533e44a3ba80e03d8837440179
ITManiac <borislarkin18@mail.ru>
2024-03-19 00:36:55 +0300

    L4_personal task added

---
+++ L4_personal task/modules/urls.js
@@ -0,0 +1,22 @@
+import {accessToken, version} from "./consts.js";
+
+class Urls {
+    constructor() {
+        this.url = 'https://api.vk.com/method'
+        this.commonInfo = `access_token=${accessToken}&v=${version}`
+    }
+
+    getUserInfo(userId) {
+        return `${this.url}/users.get?user_ids=${userId}&fields=photo_400_orig&${this.commonInfo}`
+    }
+
+    getGroupMembers(groupId) {
+        return `${this.url}/groups.getMembers?group_id=${groupId}&fields=photo_400_orig&${this.commonInfo}`
+    }
+
+    getConversationMembers(peer_id){
+        return `${this.url}/messages.getConversationMembers?peer_id=${peer_id}&${this.commonInfo}`
+    }
+}
+
+export const urls = new Urls()
