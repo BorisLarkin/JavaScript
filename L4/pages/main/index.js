2bb7cf91dd17511be752b9db3d2219d98337bd84
Borizzler <borislarkin18@mail.ru>
2024-03-18 13:45:52 +0300

    L4 started + readme

---
+++ L4/pages/main/index.js
@@ -0,0 +1,50 @@
+import {ProductCardComponent} from "../../components/product-card/index.js";
+import {ProductPage} from "../product/index.js";
+import {ajax} from "../../modules/ajax.js";
+import {urls} from "../../modules/urls.js";
+import {groupId} from "../../modules/consts.js";
+
+export class MainPage{
+    constructor(parent){
+        this.parent=parent;
+    }
+    
+    get pageRoot() {
+        return document.getElementById('main-page')
+    }
+        
+    getHTML() {
+        return (
+            `
+                <div id="main-page" class="d-flex flex-wrap"><div/>
+            `
+        )
+    }
+        
+    getData() {
+        ajax.post(urls.getGroupMembers(groupId), (data) => {
+            this.renderData(data.response.items)
+        })
+    }
+    
+    clickCard(e) {
+        const cardId = e.target.dataset.id
+    
+        const productPage = new ProductPage(this.parent, cardId)
+        productPage.render()
+    }
+    renderData(items) {
+        items.forEach((item) => {
+            const productCard = new ProductCardComponent(this.pageRoot)
+            productCard.render(item, this.clickCard.bind(this))
+        })
+    }
+
+    render() {
+        this.parent.innerHTML = ''
+        const html = this.getHTML()
+        this.parent.insertAdjacentHTML('beforeend', html)
+
+        this.getData()
+    }
+}
