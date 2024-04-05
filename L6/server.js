d57a87b5dc5195fbb3ecac7722107557956177ee
Borizzler <borislarkin18@mail.ru>
2024-04-05 18:54:43 +0300

    sth guud

--- L6/server.js
+++ L6/server.js
@@ -1,14 +1,57 @@
-const http = require("http");
+const express = require('express');
+
+const app = express(); // 1
 
 const host = 'localhost';
 const port = 8000;
 
 const handler = (req, res) => {
     res.writeHead(200);
-    res.end('Курс ПСП такой крутой!');
+    res.end('ПСП такой крутой!');
 };
 
-const server = http.createServer(handler);
-server.listen(port, host, () => {
+app.get('/music', (req, res) => {
+    res.end('Я слушаю только Radiohead!');
+});
+
+app.get('/course', (req, res) => {
+    res.json({ course: 'ПСП' });
+});
+
+app.listen(port, host, () => { // 3
     console.log(`Сервер запущен по адресу http://${host}:${port}`);
+});
+
+const readJson = (fileName) => {
+    const file = fs.readFileSync(path.join(__dirname, fileName), "utf8");
+    const json = JSON.parse(file);
+
+    return json;
+};
+
+const storageName = 'stocks.json';
+
+app.get('/stocks', (req, res) => {
+    const stocks = readJson(storageName);
+    res.send(stocks);
+});
+
+app.get('/stocks/:id', (req, res) => {
+    const id = req.params.id; // 1
+    
+    const numberId = Number.parseInt(id);
+    if (Number.isNaN(numberId)) { // 2
+        res.status(400).send({status: 'Bad Request', message: 'id must be number!'});
+    }
+
+    const stocks = readJson(storageName);
+    const stock = stocks.find((value) => { // 3
+        return value.id === numberId;
+    });
+
+    if (stock) { // 4
+        res.send(stock);
+    } else {
+        res.status(404).send({status: 'Not Found', message: `not found stock with id ${numberId}`});
+    }
 });
