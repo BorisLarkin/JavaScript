22f14c15865dd8896759c1168bc08845388e4bfa
ITManiac <borislarkin18@mail.ru>
2024-04-17 13:35:39 +0300

    ht logic change

--- HT/task.js
+++ HT/task.js
@@ -28,24 +28,54 @@ function to_string(input) { //get string from
     return output;
 }
 
+
+
 function rle_encode(input) {
     var result = [];
     var arr = getElementByType(input, "decoded"); //get normal string from.json
-    var curr_counter = 1;
+    var curr_counter = 0;
     var buffer_index = 0;
-    var rep_buffer_index = 0;
     var buffer = new Array(2*buffer_bits); //so as to be able to check sequences 2 times the len on resemblence
     var repeat_buffer = new Array(buffer_bits);
+    var rep_buffer_length=0;
 
     for (var i = 0; i <= arr.length; i++) {
         buffer[buffer_index] = arr[i];
         buffer_index++;
-        if (buffer_index === 2*buffer_bits+1) //things went south
-        {
-
+        if (rep_buffer_length=== 0){
+            if (buffer_index === 2*buffer_bits) //buffer full
+            {
+                find_rep:{
+                    for (var len=1; len < buffer_bits+1; len++){
+                        if (buffer.slice(0,len) === buffer.slice(len,len+len)){
+                            repeat_buffer = new Array(len);
+                            for (j=0;j<len;j++){repeat_buffer[j]=buffer.shift();} //first sequence gone
+                            for (k=0;k<len;k++){buffer.shift();} //second gone
+                            buffer_index -= 2*len;
+                            curr_counter+=2;
+                            rep_buffer_length = repeat_buffer.length; //take in the len of curr_rep_buffer
+                            break find_rep;
+                        }
+                    }
+                    //Place the 1 byte in result[]
+                }
+            }
         }
-        
+        else{
+            if (buffer_index>=rep_buffer_length){ 
+                if (buffer.slice(0,rep_buffer_length) === repeat_buffer) {
+                    for (z=0;z<rep_buffer_length;z++){buffer.shift();} //second gone
+                    buffer_index -= rep_buffer_length;
+                    curr_counter++;
+                }
+                else{
+                    //Place the sequence in result[]
+                    rep_buffer_length=0
+                }
+            }
+        }
     }
+    //do sth with the remnants of buffer
     return result;
 }
 
